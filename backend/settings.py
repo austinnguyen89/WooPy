3093c0bbe1f187f20e66ca682a553c652e3d352b
austinnguyen89 <austinnguyen89@gmail.com>
2023-08-09 14:34:22 -0500

    ReadMe.md

--- backend/settings.py
+++ backend/settings.py
@@ -1,14 +1,41 @@
 """Settings module for WooPy.
 
 This module contains the configuration settings for the WooPy project, including
-WooCommerce API keys and database URI.
+WooCommerce API keys and database URI. It also includes validation to ensure that
+all required environment variables are set.
+
+Environment Variables:
+    WOO_COMMERCE_API_KEY: The API key for WooCommerce integration.
+    WOO_COMMERCE_API_SECRET: The API secret for WooCommerce integration.
+    WOO_COMMERCE_URL: The URL for WooCommerce.
+    DATABASE_URI: The URI for the database connection.
 """
 
 import os
 
-WOO_COMMERCE_API_KEY = os.environ.get(
-    'WOO_COMMERCE_API_KEY', 'your_default_api_key')
-WOO_COMMERCE_API_SECRET = os.environ.get(
-    'WOO_COMMERCE_API_SECRET', 'your_default_api_secret')
-WOO_COMMERCE_URL = os.environ.get('WOO_COMMERCE_URL', 'your_default_url')
-DATABASE_URI = os.environ.get('DATABASE_URI', 'your_default_database_uri')
+
+def get_env_variable(var_name, default=None, required=False):
+    """Retrieve and validate an environment variable.
+
+    Args:
+        var_name (str): The name of the environment variable.
+        default: The default value if the environment variable is not set.
+        required (bool): Whether the environment variable is required.
+
+    Returns:
+        The value of the environment variable.
+
+    Raises:
+        ValueError: If the environment variable is required but not set.
+    """
+    value = os.environ.get(var_name, default)
+    if value is None and required:
+        raise ValueError(f"{var_name} must be set")
+    return value
+
+
+WOO_COMMERCE_API_KEY = get_env_variable('WOO_COMMERCE_API_KEY', required=True)
+WOO_COMMERCE_API_SECRET = get_env_variable(
+    'WOO_COMMERCE_API_SECRET', required=True)
+WOO_COMMERCE_URL = get_env_variable('WOO_COMMERCE_URL', required=True)
+DATABASE_URI = get_env_variable('DATABASE_URI', required=True)
--- frontend/src/components/Settings.js
+++ frontend/src/components/Settings.js
@@ -1,68 +1,46 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+import { Form, Button, Container, Row, Col } from 'react-bootstrap'; // Importing Bootstrap components for styling
 
 const Settings = () => {
-    const [apiUrl, setApiUrl] = useState('');
+    // State variables to hold the WooCommerce API Key and Secret
     const [apiKey, setApiKey] = useState('');
-    const [error, setError] = useState('');
-    const [success, setSuccess] = useState('');
+    const [apiSecret, setApiSecret] = useState('');
 
-    // Function to load existing settings from the backend
-    const loadSettings = () => {
-        const settingsUrl = '/api/settings'; // Replace with your endpoint
-
-        axios.get(settingsUrl)
-            .then((response) => {
-                setApiUrl(response.data.apiUrl);
-                setApiKey(response.data.apiKey);
-            })
-            .catch((error) => {
-                setError('An error occurred while loading settings');
-                console.error(error);
-            });
-    };
-
-    // Function to save settings to the backend
+    // Function to handle saving settings
     const handleSave = () => {
-        // Clear previous messages
-        setError('');
-        setSuccess('');
-
-        // API endpoint for saving settings
-        const settingsUrl = '/api/settings'; // Replace with your endpoint
-
-        // Request payload
-        const payload = {
-            apiUrl: apiUrl,
-            apiKey: apiKey,
-        };
-
-        // Make a PUT request to save the settings
-        axios.put(settingsUrl, payload)
-            .then((response) => {
-                // Handle successful save
-                setSuccess('Settings saved successfully');
-            })
-            .catch((error) => {
-                // Handle request error
-                setError('An error occurred while saving settings');
-                console.error(error);
-            });
+        // Logic to save settings to the database
+        // This part is currently empty and needs to be implemented
     };
-
-    // Load settings when the component mounts
-    useEffect(() => {
-        loadSettings();
-    }, []);
 
     return (
-        <div>
-            <input type="text" placeholder="API URL" value={apiUrl} onChange={(e) => setApiUrl(e.target.value)} />
-            <input type="text" placeholder="API Key" value={apiKey} onChange={(e) => setApiKey(e.target.value)} />
-            <button onClick={handleSave}>Save</button>
-            {error && <div className="error">{error}</div>}
-            {success && <div className="success">{success}</div>}
-        </div>
+        <Container className="settings-page"> {/* Container to center-align the form */}
+            <Row className="justify-content-center"> {/* Row to create a responsive layout */}
+                <Col md={6}> {/* Column to set the form width */}
+                    <h1>Settings</h1>
+                    <Form onSubmit={handleSave}> {/* Form component */}
+                        <Form.Group controlId="apiKey">
+                            <Form.Label>WooCommerce API Key:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={apiKey}
+                                onChange={(e) => setApiKey(e.target.value)} // Updating the API Key state
+                            />
+                        </Form.Group>
+                        <Form.Group controlId="apiSecret">
+                            <Form.Label>WooCommerce API Secret:</Form.Label>
+                            <Form.Control
+                                type="text"
+                                value={apiSecret}
+                                onChange={(e) => setApiSecret(e.target.value)} // Updating the API Secret state
+                            />
+                        </Form.Group>
+                        <Button variant="primary" type="submit"> {/* Button to submit the form */}
+                            Save
+                        </Button>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
